e7f1c3da88911fbc496ed0fce5d458fe684177fe
jasontsang <jasontsang.icloud@me.com>
2017-05-05 13:45:48 +0800

    Initial Commit

--- app/src/main/java/com/github/gnastnosaj/pandora/ui/activity/PandoraActivity.java
+++ app/src/main/java/com/github/gnastnosaj/pandora/ui/activity/PandoraActivity.java
@@ -127,13 +127,19 @@ public class PandoraActivity extends BaseActivity {
         Single<String> splashImageSingle;
         if (Pandora.pro) {
             GithubService githubService = Retrofit.newSimpleService(GithubService.BASE_URL, GithubService.class);
-            splashImageSingle = githubService.getJSoupDataSource(GithubService.DATE_SOURCE_JAVLIB_TAB)
+//            splashImageSingle = githubService.getJSoupDataSource(GithubService.DATE_SOURCE_JAVLIB_TAB)
+//                    .flatMap(jsoupDataSource -> jsoupDataSource.loadData())
+//                    .map(data -> data.get(new Random().nextInt(data.size() - 1)).attrs.get("url"))
+//                    .flatMap(url -> githubService.getJSoupDataSource(GithubService.DATE_SOURCE_JAVLIB_GALLERY).flatMap(jsoupDataSource -> jsoupDataSource.loadData(url)))
+//                    .flatMap(data -> Observable.fromIterable(data))
+//                    .lastOrError()
+//                    .map(data -> data.attrs.get("cover"));
+            splashImageSingle = githubService.getJSoupDataSource(GithubService.DATE_SOURCE_GIRL_ATLAS_TAB)
                     .flatMap(jsoupDataSource -> jsoupDataSource.loadData())
                     .map(data -> data.get(new Random().nextInt(data.size() - 1)).attrs.get("url"))
-                    .flatMap(url -> githubService.getJSoupDataSource(GithubService.DATE_SOURCE_JAVLIB_GALLERY).flatMap(jsoupDataSource -> jsoupDataSource.loadData(url)))
-                    .flatMap(data -> Observable.fromIterable(data))
-                    .lastOrError()
-                    .map(data -> data.attrs.get("cover"));
+                    .flatMap(url -> githubService.getJSoupDataSource(GithubService.DATE_SOURCE_GIRL_ATLAS_GALLERY).flatMap(jsoupDataSource -> jsoupDataSource.loadData(url)))
+                    .map(data -> data.get(new Random().nextInt(data.size() - 1)).attrs.get("url"))
+                    .singleOrError();
         } else {
             splashImageSingle = Retrofit.newSimpleService(GankService.BASE_URL, GankService.class)
                     .getGankData("福利", 1, 1)
