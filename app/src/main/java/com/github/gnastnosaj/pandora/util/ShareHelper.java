ba3defc09dbb1fa6ca3b0eb58c09b3e482d7da79
jasontsang <jasontsang.icloud@me.com>
2017-05-25 11:12:27 +0800

    Initial Commit

--- app/src/main/java/com/github/gnastnosaj/pandora/util/ShareHelper.java
+++ app/src/main/java/com/github/gnastnosaj/pandora/util/ShareHelper.java
@@ -11,39 +11,41 @@ import com.bilibili.socialize.share.core.shareparam.BaseShareParam;
  * Created by jasontsang on 5/25/17.
  */
 
-public class ShareHelper implements SocializeListeners.ShareListener {
-    private final static ShareHelper instance = new ShareHelper();
+public class ShareHelper {
+    private final static ShareListener shareListener = new ShareListener();
 
     public static void share(Activity activity, BaseShareParam param) {
-        BiliShare.global().share(activity, SocializeMedia.GENERIC, param, instance);
+        BiliShare.global().share(activity, SocializeMedia.GENERIC, param, shareListener);
     }
 
-    public static void share(Activity activity, BaseShareParam param, ShareHelper shareHelper) {
-        BiliShare.global().share(activity, SocializeMedia.GENERIC, param, shareHelper);
+    public static void share(Activity activity, BaseShareParam param, ShareListener shareListener) {
+        BiliShare.global().share(activity, SocializeMedia.GENERIC, param, shareListener);
     }
 
-    @Override
-    public void onStart(SocializeMedia type) {
+    public static class ShareListener implements SocializeListeners.ShareListener {
+        @Override
+        public void onStart(SocializeMedia type) {
 
-    }
+        }
 
-    @Override
-    public void onProgress(SocializeMedia type, String progressDesc) {
+        @Override
+        public void onProgress(SocializeMedia type, String progressDesc) {
 
-    }
+        }
 
-    @Override
-    public void onSuccess(SocializeMedia type, int code) {
+        @Override
+        public void onSuccess(SocializeMedia type, int code) {
 
-    }
+        }
 
-    @Override
-    public void onError(SocializeMedia type, int code, Throwable error) {
+        @Override
+        public void onError(SocializeMedia type, int code, Throwable error) {
 
-    }
+        }
 
-    @Override
-    public void onCancel(SocializeMedia type) {
+        @Override
+        public void onCancel(SocializeMedia type) {
 
+        }
     }
 }
